99ca761586e508098a9ffa281bf3ab5ffd3a7019
gaoyuan5251 <310405060@qq.com>
2020-11-05 09:26:11 +0800

    秘籍

--- src/main/java/com/bigdata/git/Dugujiujian.java
+++ src/main/java/com/bigdata/git/Dugujiujian.java
@@ -4,5 +4,6 @@ public class Dugujiujian {
     public static void main(String[] args) {
         System.out.println("紫霞神功");
         System.out.println("葵花宝典");
+        System.out.println("词共");
     }
 }
