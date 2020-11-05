21d950899c89904eba3b51d0dab8b07d1c36ce61
gaoyuan5251 <310405060@qq.com>
2020-11-05 10:01:22 +0800

    5251工作流测试3

--- src/main/java/com/bigdata/git/workFlume.java
+++ src/main/java/com/bigdata/git/workFlume.java
@@ -5,5 +5,6 @@ public class workFlume {
     public static void main(String[] args) {
         System.out.println("5251测试成功");
         System.out.println("5251测试成功2");
+        System.out.println("5251测试成功3");
     }
 }
